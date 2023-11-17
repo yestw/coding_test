12700406169865f3ff518e4ec483f1472d759100
태원 <103083087+yestw@users.noreply.github.com>
2023-11-17 16:48:41 +0900

    :sparkles: app.Module
     - 환경변수 설정
     - type orm 설정
     - graphql 설정
     - 각종 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,46 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+
+import { SurveyModule } from './survey/survey.module';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './config/typeorm.config';
+import { LoggerModule } from './logger/logger.module';
+import { QuestionModule } from './question/question.module';
+import { OptionModule } from './option/option.module';
+import { AnswerModule } from './answer/answer.module';
+import { ConfigModule, ConfigService  } from "@nestjs/config";
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: typeORMConfig,
+      inject: [ConfigService],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      formatError: (err) => {
+        console.log(err);
+        return err;
+      },
+    }),
+    SurveyModule, LoggerModule, QuestionModule, OptionModule, AnswerModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService, Logger,
+  ],
 })
-export class AppModule {}
+export class AppModule{}
