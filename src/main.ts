58d6337c648468d7cc47d19731138f1074429911
태원 <103083087+yestw@users.noreply.github.com>
2023-11-17 16:29:11 +0900

    ✨ validation설정
     - 포트 변경

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // 모든 엔드포인트가 잘못된 데이터를 수신하지 않도록 보호
+  app.useGlobalPipes(new ValidationPipe({
+    enableDebugMessages: true,
+    exceptionFactory: (errrors) => {
+      console.log(errrors);
+    }
+  }));
+  await app.listen(4000);
 }
 bootstrap();
